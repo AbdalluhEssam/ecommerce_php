57e942258b0553d2510f03ff9bf08139963729e2
abdal <abdallhesam100@gmail.com>
2023-09-12 10:07:17 +0300

    first commit

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,16 @@
+<?php 
+namespace App\Http\Controllers;
+
+class ClientController extends Controller{
+
+    public function register(){
+        echo "Register";
+    }
+
+public function login(){
+    echo "Login";
+}
+
+}
+
+?>
